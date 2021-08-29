464fc0c161123f13f1d749577a9bdfbb2892e765
Abbos <65133412+AdamsMidov@users.noreply.github.com>
2021-08-29 16:15:32 +0500

    last change -added routing footer and back

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+            <h4>For tutorial in Algoritm learning center</h4>
+            <Link  to="/">Go Back</Link>
+        </div>
+    )
+}
+
+export default About
